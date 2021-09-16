af725842db1918485ddddcc3dfe57d898b15b6db
minasoha <minasoha@yahoo.com>
2021-09-16 16:11:36 -0400

    created cypress tests

---
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+describe("User-Onboard App", () =>{
+    beforeEach(()=>{
+        cy.visit('localhost:3000');
+    })
+
+    const nameInput = () => cy.get('input[name=name]');
+    const emailInput = () => cy.get('input[name=email]');
+    const passwordInput = () => cy.get('input[name=password]');
+    const checkBox = () => cy.get('input[type=checkbox]');
+    const submitBtn = () => cy.get('button');
+
+
+
+    it('checking test', () =>{
+        expect(1 + 2 ).to.equal(3);
+    })
+
+
+    
+
+})
